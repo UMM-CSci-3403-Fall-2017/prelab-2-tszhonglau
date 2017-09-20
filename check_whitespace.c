d859c64a51e9690ed582ab9872f767435593ab94
tszhonglau <lauxx265@morris.umn.edu>
2017-09-19 23:36:39 -0500

    Final commit no errors

--- check_whitespace.c
+++ check_whitespace.c
@@ -67,7 +67,9 @@ int is_clean(char* str) {
   // greater than the second.
   result = strcmp(str, cleaned);
  
-  free(cleaned);
+  if (strlen(cleaned) != 0){
+  	free(cleaned);
+  }
   
   return result == 0;
 }
